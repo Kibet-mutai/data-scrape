840c0efa0a2de6646aff7e3fb72d5bcbdf3f8c4d
Kibet Mutai <109361613+Kibet-mutai@users.noreply.github.com>
2023-09-22 01:07:55 +0300

    Initialize categories

--- restuarants_data/restuarants_data/spiders/DeliverooFoodItems.py
+++ restuarants_data/restuarants_data/spiders/DeliverooFoodItems.py
@@ -1,10 +1,29 @@
 import scrapy
+import pandas as pd
 
 
 class DeliveroofooditemsSpider(scrapy.Spider):
-    name = "DeliverooFoodItems"
+    name = "FoodItems"
     allowed_domains = ["deliveroo.co.uk"]
-    start_urls = ["https://deliveroo.co.uk"]
+    start_urls = [
+                    "https://deliveroo.co.uk/menu/Kettering/brambleside/central-england-co-op-kettering-hallwood-road/?day=today&geohash=gcr6pmnt52wh&time=ASAP&linked_request_uuid=aac2c088-4c6b-4fff-9d24-b3083ecde256&category_id=155090856",
+                    "https://deliveroo.co.uk/menu/Kettering/brambleside/central-england-co-op-kettering-hallwood-road/?day=today&geohash=gcr6pmnt52wh&time=ASAP&category_id=204450213&linked_request_uuid=b10441e6-7f49-464d-b2d0-6c8622209834"
+                  ]
 
     def parse(self, response):
-        pass
+        all_elements = response.xpath('//div[@class="MenuItemCard-a927b3314fc88b17 MenuItemCard-1c229a9ab5bb702f"]')
+        food_items = response.xpath('//div[@class="notranslate"]/p/span/text()').extract()
+        item_price = response.xpath('//div[@class="MenuItemCard-ffce2437ec17693c"]/div/span/text()').extract()
+    
+        scraped_data =  {
+            'FOOD ITEM' : food_items,
+            'ITEM PRICE': item_price
+        }
+        self.df = pd.DataFrame(scraped_data)
+        # yield df
+
+
+    def closed(self, reason):
+        self.df.to_csv('food-items.csv', index=False)
+
+        # pass
--- restuarants_data/restuarants_data/spiders/deliveroo.py
+++ restuarants_data/restuarants_data/spiders/deliveroo.py
@@ -14,7 +14,7 @@ class DeliverooSpider(scrapy.Spider):
         self.start_urls = df['RESTUARANT LINK'].tolist()
 
 
-        self.scraped_data = pd.DataFrame(columns=['CATEGORY NAME', 'RESTUARANT URL', 'DELIVERY RADIUS'])
+        self.scraped_data = pd.DataFrame(columns=['CATEGORY NAME', 'RESTUARANT URL', 'DELIVERY RADIUS', 'CATEGORY LINK'])
 
 
 
@@ -23,14 +23,43 @@ class DeliverooSpider(scrapy.Spider):
 
         delivery_radius_match = re.search(r'([\d.]+)\s*(mile|miles)', delivery_radius_data, re.IGNORECASE)
         delivery_radius = delivery_radius_match.group(1) if delivery_radius_match else 'N/A'
+        category_links = response.xpath('//a[@class="UICard-fc546e6554c9acdb undefined UICard-469f33f83128feb3 UICard-20e6f74249484648"]/@href').extract()
+        # other_category_names = response.xpath('//div[@class="Layout-acd23e46648eee23"]/h2').extract()
+        other_category_names = response.xpath('//div[@class="Layout-acd23e46648eee23"]/h2/text()').extract()
         category_names = response.xpath('//ul[@class="UILayoutGrid-99a474f58af4cb2d"]/li/a/div/div/span[contains(@class, "ccl-0956b2f88e605eb8")]/text()').extract()
         if not category_names:
-            category_names = ['EMPTY']
+            category_names = other_category_names
+
+        # scraped_data = []
+        for category_name in category_names:
+            food_items =response.xpath('//div[@class="notranslate"]/p/text()').extract()
+            food_item_prices = response.xpath('//div[@class="MenuItemCard-bd0c8b7203436423 ccl-5cae55d5d78c131f"]/span/text()').extract()
+            food_name = ''
+            food_price = ''
+
+            for food_item, price in zip(food_items, food_item_prices):
+                # print(f'Category: {category_name}, Food Item: {food_item}, Price: {price}')
+
+                food_name =food_item
+                print(food_name)
+                food_price = food_price
+            
+
+
+                # Ensure all arrays have the same length
+        max_length = max(len(category_names), len(category_links))
+        category_names = category_names[:max_length] + [''] * (max_length - len(category_names))
+        category_links = category_links[:max_length] + [''] * (max_length - len(category_links))
+        # food_name = food_name[:max_length] + [''] * (max_length - len(food_name))
+        # food_price = food_price[:max_length] + [''] * (max_length - len(food_price))
 
         scraped_data_temp = pd.DataFrame({
             'CATEGORY NAME': category_names,
             'RESTUARANT URL': [response.url] * len(category_names),
-            'DELIVERY RADIUS': [delivery_radius] * len(category_names)
+            'DELIVERY RADIUS': [delivery_radius] * len(category_names),
+            'CATEGORY LINK': category_links,
+            'FOOD ITEM': food_name,
+            'FOOD ITEM PRICE': food_price
         })
         self.scraped_data = pd.concat([self.scraped_data, scraped_data_temp], ignore_index=True)
 
